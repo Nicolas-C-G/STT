5d97d778da715922054fbfc973d4f8a1314e3a13
Nicolas-C-G <nicolas.cartes.genta@gmail.com>
2019-05-16 14:22:39 -0400

    Creada y probada la función para extraer la información de un audio .wav

---
+++ Funciones/Extraer_senal.m
@@ -0,0 +1,15 @@
+function [t,y] = Extraer_senal(fileName)
+%EXTRAER_SEÑAL Summary of this function goes here
+%   Parameters
+%   t = signal time line
+%   y = y(t) it's wave amplitud
+%   This function extract sound signal from an specific file(.wav)
+
+info = audioinfo(fileName);
+[y,Fs] = audioread(fileName);
+
+sound(y,Fs)
+t = 0:seconds(1/Fs):seconds(info.Duration); 
+t = t(1:end-1);
+end
+
--- main.m
+++ main.m
@@ -6,3 +6,13 @@ NumChannels = 2;
 OutFileName = 'test.wav';
 TimeCapture = 5;
 captura_audio(FS,nBits,NumChannels,OutFileName,TimeCapture)
+
+[t,y] = Extraer_senal('test.wav');
+
+%%%%%%%%%%%%%%%%
+% plot signal 
+plot(t,y)
+title('señal')
+xlabel('tiempo [Seg]')
+ylabel('amplitud')
+%%%%%%%%%%%%%%%%%%
